3a7c89d8172b53117a2f9ccb4a2cd0eb640b08b2
Weerawi <weerawikramatharindu@gmail.com>
2024-02-19 01:22:30 +0530

    update

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,5 +130,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL='users.User'
+AUTH_USER_MODEL='users.User' 
+LOGIN_REDIRECT_URL = '/dashboard/'
 AUTHENTICATION_BACKENDS=['users.backends.EmailBackend']
--- users/backends.py
+++ users/backends.py
@@ -4,31 +4,31 @@ from django.db.models import Q
 
 UserModel = get_user_model()
 
-# class EmailBackend(ModelBackend):
-#     def authenticate(self,request,username=None,password=None,**kwargs):
-#         try:
-#             user = UserModel.objects.get(Q(username_iexact = username)| Q(email_iexact=username))
-#         except UserModel.DoesNotExist:
-#             #for newly created applicationt
-#             UserModel().set_password(password)
-#             return
-#         except UserModel.MultipleObjectsReturned:
-#             #prevent multiple obj return and filter first user of it
-#             user = UserModel.objects.filter(Q(username_iexact = username)|Q(email_iexact = username)).order_by('id').first()
-#         if user.check_password(password) and self.user_can_authenticate(user):
-#             return user
+class EmailBackend(ModelBackend):
+    def authenticate(self,request,username=None,password=None,**kwargs):
+        try:
+            user = UserModel.objects.get(Q(username__iexact = username)| Q(email__iexact=username))
+        except UserModel.DoesNotExist:
+            #for newly created applicationt
+            UserModel().set_password(password)
+            return
+        except UserModel.MultipleObjectsReturned:
+            #prevent multiple obj return and filter first user of it
+            user = UserModel.objects.filter(Q(username__iexact = username)|Q(email__iexact = username)).order_by('id').first()
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
         
 
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
-        except UserModel.DoesNotExist:
-            # User does not exist
-            return None
-        except UserModel.MultipleObjectsReturned:
-            # Prevent multiple objects returned and filter first user
-            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
+# class EmailBackend(ModelBackend):
+#     def authenticate(self, request, username=None, password=None, **kwargs):
+#         try:
+#             user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+#         except UserModel.DoesNotExist:
+#             # User does not exist
+#             return None
+#         except UserModel.MultipleObjectsReturned:
+#             # Prevent multiple objects returned and filter first user
+#             user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+#         if user.check_password(password) and self.user_can_authenticate(user):
+#             return user
